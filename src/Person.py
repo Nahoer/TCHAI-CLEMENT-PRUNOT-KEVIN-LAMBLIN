309f7ef642827d76c4c302efd510442b43b9d90f
Kevin Lamblin <kevin.lamblin@orange.fr>
2022-11-09 12:09:48 +0100

    [V1] Initialisation de la classe Person

---
+++ src/Person.py
@@ -0,0 +1,9 @@
+class Person:
+    id = 0
+    name = ""
+    first_name = ""
+
+    def __init__(self, name, first_name, id):
+        self.id = id
+        self.name = name
+        self.first_name = first_name
