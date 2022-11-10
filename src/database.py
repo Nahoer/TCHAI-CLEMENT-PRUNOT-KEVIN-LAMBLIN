52499a864e6d643a56ef2f89fa3c249cd1919475
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-11-10 17:29:54 +0100

    add and insert person and transaction in db

--- src/database.py
+++ src/database.py
@@ -1,19 +1,54 @@
 import sqlite3
-import Person
+from typing import List
+from model.PersonModel import PersonModel as Person
+from model.DealModel import DealModel as Deal
 class DataBase:
     def __init__(self, path):
         try:
             self.connection = sqlite3.Connection(path)
         except sqlite3.Error as error:
             print("Failed to insert data into sqlite table", error)
-    def addPerson(self, personne:Person):
+    def addPerson(self, last_name, first_name):
+        try:
+            cursor = self.connection.cursor()
+            sqlite_insert_query = """INSERT INTO Personne
+                                              (last_name, first_name) 
+                                               VALUES
+                                              ('{}','{}')""".format(last_name, first_name)
+            count = cursor.execute(sqlite_insert_query)
+            self.connection.commit()
+            print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+            cursor.close()
+        except sqlite3.Error as error:
+            print("Erreur lors de l'insertion de la personne: ", error)
+    def getPersonList(self)->List[Person]:
         cursor = self.connection.cursor()
-        print("Successfully Connected to SQLite")
-        sqlite_insert_query = """INSERT INTO SqliteDb_developers
-                                          (id, name, email, joining_date, salary) 
-                                           VALUES 
-                                          (1,'James','james@pynative.com','2019-03-17',8000)"""
-        count = cursor.execute(sqlite_insert_query)
-        self.connection.commit()
-        print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
-        cursor.close()
+        cursor.execute("SELECT * FROM Personne")
+        rows = cursor.fetchall()
+        personList = []
+        for row in rows:
+            personList += [Person(row[1], row[2], row[0])]
+        return personList
+
+    def addTransaction(self, id_envoyeur, id_receveur, montant, date):
+        try:
+            cursor = self.connection.cursor()
+            sqlite_insert_query = """INSERT INTO Transactions
+                                              (id_envoyeur, id_receveur, montant, date) 
+                                               VALUES
+                                              ('{}','{}')""".format(id_envoyeur, id_receveur, montant, date)
+            count = cursor.execute(sqlite_insert_query)
+            self.connection.commit()
+            print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+            cursor.close()
+        except sqlite3.Error as error:
+            print("Erreur lors de l'insertion de la transaction: ", error)
+
+    def getDealList(self)->List[Deal]:
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM Transactions")
+        rows = cursor.fetchall()
+        dealList = []
+        for row in rows:
+            dealList += [Person(row[1], row[2], row[0])]
+        return dealList
