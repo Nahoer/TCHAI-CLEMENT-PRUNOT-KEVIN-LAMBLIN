f84b5dd15688da660f5f8db21deabed5120e9d8f
Kevin Lamblin <kla@atolcd.com>
2022-11-28 16:02:38 +0100

    [V1] Passage de Person en Model

--- src/model/PersonModel.py
+++ src/model/PersonModel.py
@@ -3,7 +3,7 @@ class PersonModel:
     name = ""
     first_name = ""
 
-    def __init__(self, id=-1, name="", first_name=""):
+    def __init__(self, id, name, first_name):
         self.id = id
         self.name = name
         self.first_name = first_name
