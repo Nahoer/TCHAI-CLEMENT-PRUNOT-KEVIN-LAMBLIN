e418cc4a5d353ede45e0a4695aaaac5d25b80980
Kevin Lamblin <kla@atolcd.com>
2022-11-28 16:01:38 +0100

    [V1] Initialisation de la classe Person

--- src/Person.py
+++ src/Person.py
@@ -3,7 +3,7 @@ class Person:
     name = ""
     first_name = ""
 
-    def __init__(self, name, first_name, id):
+    def __init__(self, id=-1, name="", first_name=""):
         self.id = id
         self.name = name
         self.first_name = first_name
