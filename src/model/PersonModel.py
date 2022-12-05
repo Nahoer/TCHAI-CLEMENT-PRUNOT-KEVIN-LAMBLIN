dd145f614676bd8fe34cd256fc2ec9abe5f98b33
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-12-05 23:55:20 +0100

    Update PersonModel.py
    Change "name" attribute to "last_name"

--- src/model/PersonModel.py
+++ src/model/PersonModel.py
@@ -1,9 +1,9 @@
 class PersonModel:
 
-    def __init__(self, id:int, name:str, first_name:str):
+    def __init__(self, id:int, last_name:str, first_name:str):
         self.id = id
-        self.name = name
+        self.last_name = last_name
         self.first_name = first_name
 
     def __str__(self):
-        return str(self.id)+"|"+str(self.first_name)+"|"+str(self.name)
+        return "["+str(self.id)+"] "+str(self.first_name)+" "+str(self.last_name)
