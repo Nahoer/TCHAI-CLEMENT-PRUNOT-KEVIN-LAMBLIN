e31112d8a9576d146e76c1d72e809ecf6adf9f35
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-12-06 12:04:03 +0100

    Add checkParams function + listerTransactions function

--- src/app.py
+++ src/app.py
@@ -4,44 +4,57 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+def checkParams(requestArgs, list:[str]):
+    ok = True
+    for param in list:
+        if not (param in requestArgs):
+            ok=False
+    return ok
 
 @app.route('/addTransaction')
 def addTransaction():
     db = DataBase("../database/transactions.db")
-    idEnvoyeur = int(request.args.get("idSender"))
-    idReceveur = int(request.args.get("idReceiver"))
-    montant = int(request.args.get("amount"))
-    date = request.args.get("date")
     message = ""
-    if(idEnvoyeur=="" or idReceveur=="" or montant=="" or date==""):
-        message = "Veuillez founir les données suivante :\n"
-        message += "idEnvoyeur: id de l'auteur de la transaction\n"
-        message += "idReceveur: personne qui reçoit l'argent de la transaction\n"
-        message += "montant: montant de la transaction\n"
-        message += "date: date de la transaction"
-    else:
+    if checkParams(request.args, ['idEnvoyeur', 'idReceveur', 'montant', 'date']):
+        idEnvoyeur = int(request.args.get("idSender"))
+        idReceveur = int(request.args.get("idReceiver"))
+        montant = int(request.args.get("amount"))
+        date = request.args.get("date")
         db.addTransaction(idEnvoyeur, idReceveur, montant, date)
-        print(str(idEnvoyeur)+" "+str(idReceveur)+" "+str(montant)+" "+date)
-        message = "Transaction ajoutée"
-        liste = db.getDealList()
-        for deal in liste:
-            print(deal)
+        print(str(idEnvoyeur) + " " + str(idReceveur) + " " + str(montant) + " " + date)
+        return redirect(f"/Transactions")
+    else:
+        message = "Veuillez founir les données suivante :<br/>"
+        message += "idEnvoyeur: id de l'auteur de la transaction<br/>"
+        message += "idReceveur: personne qui reçoit l'argent de la transaction<br/>"
+        message += "montant: montant de la transaction<br/>"
+        message += "date: date de la transaction"
+        return message
+
+@app.route('/Transactions')
+def listerTransactions():
+    db = DataBase("../database/transactions.db")
+    liste = db.getDealList()
+    message=""
+    for deal in liste:
+        message+=str(deal)+"<br/>"
     return message
 
 @app.route('/addPerson')
 def addPersonne():                                  #/addPerson?firstName=<firstname>&lastName=<lastname> sans quote pour ajouter
     db = DataBase("../database/transactions.db")
-    first_name = str(request.args.get("firstName"))
-    last_name = str(request.args.get("lastName"))
+
     message = ""
-    if not first_name or not last_name:
-        message = "Veuillez founir les données suivante :\n"
-        message += "firstName: id de l'auteur de la transaction\n"
-        message += "lastName: personne qui reçoit l'argent de la transaction"
+    if checkParams(request.args, ['lastName', 'firstName']):
+        first_name = str(request.args.get("firstName"))
+        last_name = str(request.args.get("lastName"))
+        db.addPerson(last_name, first_name)
+        return redirect(f"/Persons")
     else:
-        db
-        #db.addPerson(last_name, first_name)
-    return redirect(f"/Persons")
+        message = "Veuillez founir les données suivante :<br/>"
+        message += "firstName: id de l'auteur de la transaction<br/>"
+        message += "lastName: personne qui reçoit l'argent de la transaction"
+        return message
 
 
 @app.route('/Persons')
