5f939bb58aea646bbec1092a9de8f77b7fd684d7
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-12-11 21:47:38 +0100

    update wip

--- src/asymetric_crypto.py
+++ src/asymetric_crypto.py
@@ -1,4 +1,7 @@
 import rsa
+import hashlib
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+from Crypto.Hash import BLAKE2b
 class Asymetric_Crypto:
 
     def __init__(self, content:str, public_key:str, crypto_algorithm:str):
@@ -7,21 +10,31 @@ class Asymetric_Crypto:
         self.public_key = public_key
 
     def get_signature(self, private_key):
-        return rsa.sign(self.content, private_key, self.crypto_algorithm)
+        signer = PKCS115_SigScheme(private_key)
+        return signer.sign(self.content)
     def get_content(self):
         return self.content.decode("utf-8")
 
     def encrypt(self):
-        self.content = rsa.encrypt(self.content, self.public_key)
-
+        try:
+            hash = BLAKE2b.new()
+            hash.update(self.content)
+            self.content = hash
+        except Exception as exception:
+            print(exception)
     def decrypt(self, private_key):
         try:
             self.content = rsa.decrypt(self.content, private_key)
-        except:
+        except Exception as exception:
+            print(exception)
             return False
     def verify_signature(self, private_key):
-        try: return rsa.verify(self.content, self.get_signature(private_key), private_key) == self.crypto_algorithm
-        except: return False
+        try:
+            check = PKCS115_SigScheme(private_key).verify(self.content, self.get_signature(private_key))
+            return check
+        except Exception as exception:
+            print(exception)
+            return False
 
 
 
