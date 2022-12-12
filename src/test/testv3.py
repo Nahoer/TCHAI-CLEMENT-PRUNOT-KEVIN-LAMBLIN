f12e57b29c85eb0c88f9b28e149fcc75e7d0ffad
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-12-12 22:55:20 +0100

    update previous attacks

---
+++ src/test/testv3.py
@@ -0,0 +1,30 @@
+import testv1
+import requests
+import json
+
+base_url = "http://127.0.0.1:5000"
+with open('../../utils/config.json', 'r') as openfile:
+    json_object = json.load(openfile)
+
+with open('../../utils/config.json', 'w') as openfile:
+    json_object["database"] = "test"
+    json.dump(json_object, openfile)
+
+# Make a connexion test with the flask server
+r = requests.get(base_url + "/connexion")
+
+if r.text == "Connexion OK":
+    # Show the data integrity
+    r = requests.get(base_url + "/verifyIntegrity")
+    print("Vérificiation de l'intégrité : ")
+    print(r.text)
+    print()
+
+    # Call the v2 attack
+    print("Attaque : ")
+    from src.attack import attackv2
+
+    # Show the data integrity after attack
+    r = requests.get(base_url + "/verifyIntegrity")
+    print("Vérificiation de l'intégrité : ")
+    print(r.text)
