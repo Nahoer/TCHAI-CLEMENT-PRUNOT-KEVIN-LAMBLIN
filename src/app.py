69f8379b913db9e29365a6922dec820761132878
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-12-12 22:16:24 +0100

    Asymetric crypto

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
+import binascii
+
 from flask import Flask, request
 from Crypto.PublicKey import RSA
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+from Crypto.Hash import BLAKE2b
 from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from asymetric_crypto import Asymetric_Crypto
 from database import DataBase
@@ -13,7 +17,7 @@ def getConfig():
     configfile = json.load(open("../utils/config.json"))
     database = json.load(open("../utils/databases.json"))
     config = {}
-    #config["keys_path"] = configfile["keys"]
+    config["keys_path"] = configfile["keys"]
     config["db_path"] = database[configfile["database"]]
     return config
 def checkParams(requestArgs, list: [str]):
@@ -26,42 +30,53 @@ def checkParams(requestArgs, list: [str]):
 def dbConnexion():
     return DataBase(getConfig()["db_path"])
 def fonctionHachage(string:bytes):
-    return hashlib.md5(string)
+    h = BLAKE2b.new()
+    h.update(string)
+    return h
+@app.route('/dev/makeSignature')
+def makeSignature():                                                                 #/!\ Only for dev environment /!\#
+    if checkParams(request.args, ['idSender', 'idReceiver', 'amount']):
+        db = dbConnexion()
+        idEnvoyeur = request.args.get("idSender")
+        idReceveur = int(request.args.get("idReceiver"))
+        montant = float(request.args.get("amount"))
+        with open(getConfig()["keys_path"], 'r') as file:
+            json_object = json.load(file)
+        public_key = RSA.import_key(json_object[idEnvoyeur]["publicKey"])
+        private_key = RSA.import_key(json_object[idEnvoyeur]["privateKey"])
+        date = datetime.date.today()
+        deal_list = db.getDealList()
+        last_hash = deal_list[len(deal_list) - 1].h
+        totalstr = str(idEnvoyeur) + str(idReceveur) + str(montant) + str(date) + str(last_hash)
+        current_hash = fonctionHachage(totalstr.encode("utf-8"))
+        signer = PKCS115_SigScheme(private_key)
+        signature = signer.sign(current_hash)
+        return {"signature":binascii.hexlify(signature).decode("utf-8")}
+
+                                                                                     #--------------------------------#
 @app.route('/transactions/add')
 def addTransaction():
     db = dbConnexion()
-    if checkParams(request.args, ['idSender', 'idReceiver', 'amount', "privateKey"]):
+    if checkParams(request.args, ['idSender', 'idReceiver', 'amount', "signature"]):
         idEnvoyeur = int(request.args.get("idSender"))
         idReceveur = int(request.args.get("idReceiver"))
         montant = float(request.args.get("amount"))
-
+        signature = request.args.get("signature")
+        signature = binascii.unhexlify(signature.encode("utf-8"))
         public_key = RSA.import_key(db.getPerson(int(idEnvoyeur)).public_key)
-        private_key = "-----BEGIN RSA PRIVATE KEY-----\nMIICXAIBAAKBgQCGM3CeLKg2DtWh0qVXSnLndrH9frPMeUAELuSF61XNlOQq/TRo\nGe1lNVYoNUE03dUqV/Yf92j+Wzcp7hu6IPGNE70/fthQm5KiBVgMUhgDDw4uRmRO\n9gBV2H0FsHanzTduaqkT7mObVdE/WBnYJEd48wAzAxRZdylpR2lqNCbgMwIDAQAB\nAoGACXh93RvHrzDy0J29/2AIpd8RhpM3exmfc+wfQnguMPjt9T6zQRl7UGYoM2Q2\nutVg4sEcZHsgVLr5NKNSH5uNkE4HAE6dmgSfEdb50jSwFCQa954IMomhYzxsjrcs\nLtM8mimBzAOlFgEiGf9nyjmJYp3/a/cFV1vG4mtFx14FYVkCQQC2J0eJBvmiDrta\nzpL6Zc/kIoEfXXNJ0vNYpEAfpH7qTh+LSzAyHOBeQ+Fj07WRthAAj6okbbDNnCAD\nvslCVsdFAkEAvJtuC73ug6rXxi0VIOHsFho+AckcHG8YHhuStncphwEkd6P9q7h/\nLOexc4nZqB8LqKdx/DESUk4CPWafLzYlFwJAL+2N6QQo0vdFXNNV4QTA+qoJh5Mz\nLo2O8hflt2205zm/Gwuhls36S1NZDsc50ykwdLVYc1VZXABkfBfLJOVocQJBAI+6\nxw8Nq7ENRagRfRN096wUTYKg1tpYUwHGs3R3tN7cIQVHpK3zSH9ZBaLtvz/egM0C\n5dtxLewo5I4UIWUiFvsCQB3tzW4+x5m4QCbfriPq6+sgMJ3CXAmUvRPrdprSbLMc\n+aICAcEkBOPLvnI6DRc9UiHfy+lGnbNEnze8PE83bnw=\n-----END RSA PRIVATE KEY-----"
-        param_private_key = request.args.get("privateKey")
-        """for i in range(len(param_private_key)):
-            if param_private_key[i] == " " and i>31 and i < len(param_private_key)-30:
-                private_key+="+"
-            else:
-                private_key+= param_private_key[i]"""
-        """print(len(private_key))
-        print(private_key)
-        print(db.getPerson(int(idEnvoyeur)).public_key)"""
-        private_key = RSA.import_key(private_key) #crash
 
-        date = datetime.date.today()
-        deal_list = db.getDealList()
-
-        last_hash = deal_list[len(deal_list)-1].h
-        totalstr = str(idEnvoyeur)+str(idReceveur)+str(montant)+str(date)+str(last_hash)
-
-        current_hash = fonctionHachage(totalstr.encode("utf-8")).hexdigest()
-        crypteur = Asymetric_Crypto(current_hash, public_key, "md5")
-        crypteur.encrypt()
-        if crypteur.verify_signature(private_key):
-            #db.addTransaction(idEnvoyeur, idReceveur, montant, date, current_hash)
+        try:
+            date = datetime.date.today()
+            deal_list = db.getDealList()
+            last_hash = deal_list[len(deal_list) - 1].h
+            totalstr = str(idEnvoyeur) + str(idReceveur) + str(montant) + str(date) + str(last_hash)
+            current_hash = fonctionHachage(totalstr.encode("utf-8"))
+            verifieur = PKCS115_SigScheme(public_key)
+            verifieur.verify(current_hash, signature)
+            db.addTransaction(idEnvoyeur, idReceveur, montant, date, current_hash)
             message = "transaction ajoutée"
-        else:
-            message = "Erreur de signature"
+        except Exception as exception:
+            message = exception.__str__()
         return message
     else:
         message = "Veuillez founir les données suivante :<br/>"
@@ -89,16 +104,27 @@ def getTransaction(idTransaction):
 def addPersonne():  # /persons/add?firstName=<firstname>&lastName=<lastname> sans quote pour ajouter
     db = dbConnexion()
     #Créations des clé publique et privée
-
-
     if checkParams(request.args, ['lastName', 'firstName']):
         first_name = str(request.args.get("firstName"))
         last_name = str(request.args.get("lastName"))
+
+        #RSA keys
         keys = RSA.generate(1024)
         private_key = keys.exportKey('PEM')
         public_key = keys.publickey().exportKey('PEM')
+        #add to database
         db.addPerson(last_name, first_name, public_key.decode())
-        return {"privateKey": private_key.decode()}
+        #---add keys to jsonfile /!\ only for dev environment---#
+        list = db.getPersonList()
+        personAdded = list[len(list)-1]
+        with open(getConfig()["keys_path"], 'r') as file:
+            json_object = json.load(file)
+        keysDict = {"privateKey": private_key.decode(), "publicKey": public_key.decode()}
+        json_object[str(personAdded.id)] = keysDict
+        with open(getConfig()["keys_path"], 'w') as file:
+            json.dump(json_object, file)
+        #-------------------------------------------------------#
+        return keysDict
     else:
         message = "Veuillez founir les données suivante :<br/>"
         message += "firstName: Prénom de la personne<br/>"
